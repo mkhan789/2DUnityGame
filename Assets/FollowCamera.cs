0d71f5be52a57a92bee156dc1240ed7f88c36fb6
mkhan789 <52898031+mkhan789@users.noreply.github.com>
2022-02-16 22:10:48 -0500

    adding comments

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    // Gives the position of player
     public Transform target;
+
+    // Initialize offset
     public Vector3 offset;
 
     private void FixedUpdate() 
     {
+        // Adding the two vectors to the position of the camera
         transform.position = target.position+offset;
     
     }
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,33 +8,44 @@ public class Movement : MonoBehaviour
     float horizontalMove = 0;
     public float runSpeed = 4f;
 
+    // Create header Component
     [Header("Component")]
+    // Initialize Rigidbody2D
     public Rigidbody2D Rb;
+
+    // Initialize LayerMask
     public LayerMask groundLayer;
 
-
+    // Creadt header Collision
     [Header("Collision")]
+
+    // Initialize onGround variable to be false
     public bool onGround = false;
+
+    // Initialize the length of the line that checks the collision of the ground
     public float groundLine = 2;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize Rigidbody2D on Start function
         Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Checks the ground by using Raycast to see if there are collisions with colliders which will then return true
         onGround = Physics2D.Raycast(transform.position, Vector2.down, groundLine, groundLayer);
-
+        
+        // Input keys for character movement
         bool space = Input.GetKey(KeyCode.Space);
         bool rightKey = Input.GetKey(KeyCode.RightArrow);
         bool leftKey = Input.GetKey(KeyCode.LeftArrow);
 
         
-
+        // When right key is pressed, character will move right
         if (rightKey)
         {
             Debug.Log("Right Key Pressed");
@@ -42,6 +53,8 @@ public class Movement : MonoBehaviour
 
         }
 
+
+        // When left key is pressed, character will move right
         if (leftKey)
         {
             Debug.Log("Left Key Pressed");
@@ -49,7 +62,7 @@ public class Movement : MonoBehaviour
 
         }
 
-
+        // When space key is pressed, character will jump off the ground, but also has to standing on the ground
         if (space && onGround)
         {
             Debug.Log("Space Key Pressed");
@@ -74,7 +87,10 @@ public class Movement : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        // The color of the raycast line is blue
         Gizmos.color = Color.blue;
+
+        // The DrawLine function draw a line where the raycast is
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down*groundLine);
     }
 }
